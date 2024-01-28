2245e8ba0169c2e662d99a24753478419098104f
3waffel <45911671+3waffel@users.noreply.github.com>
2024-01-28 18:15:18 +0100

    clean flake files

--- flake.nix
+++ flake.nix
@@ -2,28 +2,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
-    flake-compat,
   }:
     utils.lib.eachSystem ["aarch64-linux" "x86_64-linux"] (
       system: let
-        project-name = "shader-showcase";
-
         pkgs = import nixpkgs {inherit system;};
         inherit (pkgs) lib stdenv;
-        commonNativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
 
+        project-name = "shader-showcase";
         godot-version = pkgs.godot-headless.version;
         export-templates = let
           unpatched = pkgs.fetchzip {
@@ -35,20 +26,20 @@
           pkgs.stdenv.mkDerivation {
             pname = "godot-export-templates";
             version = godot-version;
-            buildInputs = with pkgs; [
-              autoPatchelfHook
-              xorg.libXcursor
-              xorg.libXinerama
-              xorg.libXext
-              xorg.libXrandr
-              xorg.libXi
-              libglvnd
-            ];
             dontUnpack = true;
             installPhase = ''
               cp -r ${unpatched} $out
             '';
           };
+        configurePhase = ''
+          mkdir -p "$TMP/.config"
+          mkdir -p "$TMP/.local/share/godot/templates"
+          mkdir -p "$TMP/.config/godot/projects/"
+          export HOME=$TMP
+          export XDG_CONFIG_HOME="$TMP/.config"
+          export XDG_DATA_HOME="$TMP/.local/share"
+          ln -s ${export-templates} "$TMP/.local/share/godot/templates/${godot-version}.stable"
+        '';
       in rec {
         packages.default = packages.linux64;
 
@@ -59,20 +50,14 @@
             buildInputs = [
               godot-headless
             ];
-            phases = ["buildPhase"];
+            inherit configurePhase;
             buildPhase = ''
-              mkdir -p "$TMP/.config"
-              mkdir -p "$TMP/.local/share/godot/templates"
-              mkdir -p "$TMP/.config/godot/projects/"
-              export HOME=$TMP
-              export XDG_CONFIG_HOME="$TMP/.config"
-              export XDG_DATA_HOME="$TMP/.local/share"
-              ln -s ${export-templates} "$TMP/.local/share/godot/templates/${godot-version}.stable"
-
               cp -r $src $TMP/src
               chmod -R u+w -- "$TMP/src"
               mkdir -p "$TMP/src/build/linux"
               godot-headless -v --path "$TMP/src" --export "Linux/X11" build/linux/${project-name}.x86_64
+            '';
+            installPhase = ''
               mv $TMP/src/build $out
             '';
             dontStrip = true;
@@ -85,20 +70,14 @@
             buildInputs = [
               godot-headless
             ];
-            phases = ["buildPhase"];
+            inherit configurePhase;
             buildPhase = ''
-              mkdir -p "$TMP/.config"
-              mkdir -p "$TMP/.local/share/godot/templates"
-              mkdir -p "$TMP/.config/godot/projects/"
-              export HOME=$TMP
-              export XDG_CONFIG_HOME="$TMP/.config"
-              export XDG_DATA_HOME="$TMP/.local/share"
-              ln -s ${export-templates} "$TMP/.local/share/godot/templates/${godot-version}.stable"
-
               cp -r $src $TMP/src
               chmod -R u+w -- "$TMP/src"
               mkdir -p "$TMP/src/build/windows"
               godot-headless -v --path "$TMP/src" --export "Windows Desktop" build/windows/${project-name}.exe
+            '';
+            installPhase = ''
               mv $TMP/src/build $out
             '';
             dontStrip = true;
@@ -111,20 +90,14 @@
             buildInputs = [
               godot-headless
             ];
-            phases = ["buildPhase"];
+            inherit configurePhase;
             buildPhase = ''
-              mkdir -p "$TMP/.config"
-              mkdir -p "$TMP/.local/share/godot/templates"
-              mkdir -p "$TMP/.config/godot/projects/"
-              export HOME=$TMP
-              export XDG_CONFIG_HOME="$TMP/.config"
-              export XDG_DATA_HOME="$TMP/.local/share"
-              ln -s ${export-templates} "$TMP/.local/share/godot/templates/${godot-version}.stable"
-
               cp -r $src $TMP/src
               chmod -R u+w -- "$TMP/src"
               mkdir -p "$TMP/src/build/web"
               godot-headless -v --path "$TMP/src" --export "HTML5" build/web/index.html
+            '';
+            installPhase = ''
               mv $TMP/src/build/web $out
             '';
             dontStrip = true;
@@ -132,19 +105,10 @@
 
         devShells.default = with pkgs;
           mkShell {
-            buildInputs = [
+            packages = [
               godot-headless
             ];
-            nativeBuildInputs = commonNativeBuildInputs;
-            shellHook = ''
-              mkdir -p "$TMP/.config"
-              mkdir -p "$TMP/.local/share/godot/templates"
-              mkdir -p "$TMP/.config/godot/projects/"
-              export HOME=$TMP
-              export XDG_CONFIG_HOME="$TMP/.config"
-              export XDG_DATA_HOME="$TMP/.local/share"
-              ln -s ${export-templates} "$TMP/.local/share/godot/templates/${godot-version}.stable"
-            '';
+            inherit configurePhase;
           };
       }
     );
